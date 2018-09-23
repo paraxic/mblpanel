b6eae7bb0147e0eb715f063a2d55ef0504e6ddd5
Paraxic <paraxic@outlook.com>
2018-09-23 19:21:13 -0400

    Version 1.01, added util subdir,bump.sh
    Signed-off-by: Paraxic <paraxic@outlook.com>

--- util/bump.sh
+++ util/bump.sh
@@ -6,5 +6,6 @@ files=( "./lib/common" "./lib/clock" "./lib/bspwm" "./lib/net" "./mblpanel")
 for _file_ in ${files[@]}
  do
   echo $_file_
-  cat $_file_ | sed "s|[0-9]\.[0-9][0-9]|$1|" > $_file_
+  cat $_file_ | sed "s|Version\:.*|Version\: $1|" > $_file_.new
+  rm $_file_ && mv $_file_.new $_file_
 done
